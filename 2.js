bba4944ec22c6debddf7a05f1137e3009ff0ad58
Mursalin Islam <mursalinir@gmail.com>
2022-07-26 21:16:22 +0600

    update

--- 2.js
+++ 2.js
@@ -4,12 +4,8 @@ const takeInput = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-takeInput.question("", function (answer) {
-    input = parseInt(answer)
-    takeInput.close();
-});
 
-const n = input;
+const n = dataInput();
 let d;
 let max_x = 0;
 let min_x = 10000;
@@ -28,15 +24,29 @@ let cristmasTree = [
     }
 ]
 
-/*for(var i=0; i<n; i++) {
+for(var i=0; i<n; i++) {
 
-	cristmasTree[i].tree.x = prompt();
-    cristmasTree[i].tree.y = prompt();
-    cristmasTree[i].tree.r = prompt();
-}*/
-
+	cristmasTree[i].tree.x = dataInput();
+    cristmasTree[i].tree.y = dataInput();
+    cristmasTree[i].tree.r = dataInput();
+}
+console.log(cristmasTree)
 const printMyValue = () => {
     console.log("Hello world, This is your number", number)
 }
 
+/*const distance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}*/
+function dataInput(){
+    takeInput.question("", function (answer) {
+        input = parseInt(answer)
+        takeInput.close();        
+    });
+    return input;
+}
+
+function distance (x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
 //console.log(distance())
