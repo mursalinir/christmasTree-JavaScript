1735e12e8f8406b6001f9a3170e898c935befc40
Mursalin Islam <mursalinir@gmail.com>
2022-07-26 20:17:34 +0600

    cristmas illumination problem

---
+++ 1.js
@@ -0,0 +1,50 @@
+let number;
+let n = 0; // which value will change 
+const jira = 4; //which value will not change
+let tree = [];
+let cristmasTree = [
+    {
+        tree:
+        {
+            X: 0,
+            y: 0,
+            r: 0
+        }
+    }
+]
+let value;
+value = {
+    x: 10,
+    y: 12,
+    r: 10
+}
+let value2 = {
+    x: 11,
+    y: 13,
+    r: 10
+}
+cristmasTree.push({
+    tree: value
+});
+cristmasTree.push({
+    tree: value2
+});
+//console.log()
+
+const distance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
+const printMyValue = () => {
+    console.log("Hello world, This is your number", number)
+}
+const readline = require("readline");
+const takeInput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+takeInput.question("Please enter the number value ", function (answer) {
+    number = parseInt(answer)
+    printMyValue()
+    takeInput.close();
+});
+console.log(distance())
---
+++ 2.js
@@ -0,0 +1,46 @@
+let input;
+const readline = require("readline");
+const takeInput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+takeInput.question("", function (answer) {
+    input = parseInt(answer)
+    takeInput.close();
+});
+
+const n = input;
+let d;
+let max_x = 0;
+let min_x = 10000;
+let counter = 0;
+let max_tree = 0; 
+
+const distance = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
+
+let tree = [];
+let cristmasTree = [
+    {
+        tree:
+        {
+            x: 0,
+            y: 0,
+            r: 0
+        }
+    }
+]
+
+/*for(var i=0; i<n; i++) {
+
+	cristmasTree[i].tree.x = prompt();
+    cristmasTree[i].tree.y = prompt();
+    cristmasTree[i].tree.r = prompt();
+}*/
+
+const printMyValue = () => {
+    console.log("Hello world, This is your number", number)
+}
+
+console.log(distance())
