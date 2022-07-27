01dc9fb600cf8abd17096165382c398046134242
Mursalin Islam <mursalinir@gmail.com>
2022-07-27 22:06:08 +0600

    a

--- final.js
+++ final.js
@@ -5,14 +5,13 @@ const takeInput = readline.createInterface({
 });
 let number
 let d;
-let max_x = 0;
-let min_x = 10000;
+let max_x = 10000;
+let min_x = 0;
 let counter = 0;
 let max_tree = 0;
 let x;
 let y;
 let r;
-const tree = [];
 let christmasTree = []
 let value = {
     x: 0,
@@ -30,25 +29,43 @@ function inputTree(n) {
             value.y = parseInt(y);
             takeInput.question('Please input r: ', (r) => {
                 value.r = parseInt(r);
-                christmasTree.push({x: value.x, y: value.y, r: value.r});
+                christmasTree.push({ x: value.x, y: value.y, r: value.r });
                 if (n > 0) {
                     inputTree(n - 1);
                 } else {
-                    takeInput.close();
+                    //takeInput.close();
+                    maxChristmasTree();
                 }
             })
         })
     });
-    //treeDekhao();
+    treeDekhao();
+}
+function maxChristmasTree() {
+    for (var i = min_x; i <= max_x; i++) {
+        counter = 0;
+        for (var j = 0; j < number; j++) {
+            d = distance(i, 0, christmasTree[j].x, christmasTree[j].y);
+            if (d <= christmasTree[j].r)
+                counter++;
+        }
+        if (counter > max_tree) {
+            max_tree = counter;
+        }
+    }
+    show();
+}
+function show() {
+    console.log(max_tree)
 }
 
-function dekhao(n) {
-    console.log(n)
+function treeDekhao() {
+    console.log(christmasTree)
 }
 function distance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 }
 //console.log(distance())
-christmasTree.map(tree =>{
+christmasTree.map(tree => {
     console.log(tree.x)
 })
